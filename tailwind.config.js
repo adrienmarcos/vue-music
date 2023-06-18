c33e2eb246fcfca8bd11342e0b822617d3d023a9
Adrien Rossato <adrien.marcos@gmail.com>
2023-06-18 19:56:04 -0300

    feat: add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        raleway: "'Raleway', 'sans-serif'",
+        lora: "'Lora', 'serif'",
+        dm: "'DM Mono', 'monospace'"
+      }
+    }
   },
   plugins: []
 }
