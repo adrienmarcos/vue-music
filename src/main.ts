05255d39a7fa419c594de6e4cd3cbe90e52d0d9c
Adrien Rossato <adrien.marcos@gmail.com>
2023-06-18 17:06:17 -0300

    chore: install tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import './assets/base.css'
 
 const app = createApp(App)
 
