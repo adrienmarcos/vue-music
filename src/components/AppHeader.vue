ac94e608d71d94adf2c4b644e04bf90c6823549b
Adrien Rossato <adrien.marcos@gmail.com>
2023-06-18 21:18:01 -0300

    chore: add VeeValidate plugin

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -31,7 +31,6 @@ export default {
   methods: {
     toggleAuthModal(): void {
       this.isOpen = !this.isOpen
-      console.log(this.isOpen)
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
+import veeValidatePlugin from './includes/validation'
 
 import './assets/base.css'
 import './assets/main.css'
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(veeValidatePlugin)
 
 app.mount('#app')
